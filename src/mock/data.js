cb626ca511632f0d05a1fe59c1b809b891a0146d
gustavboyd <boydgustav@gmail.com>
2021-04-04 19:05:34 -0700

    message

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,55 +9,43 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hi, we are ',
+  name: 'Final Iteration',
+  subtitle: 'Software Developers',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'We are the group Final Iteration comprised of senior Computer Science students at Sacramento State.',
+  paragraphTwo: 'This project is being developed for Digital Davis for UC Davis Health. Digital Davis needs a computerized ' + 
+  'management system for the Innovation Center. The system would manage analyzing foot traffic and calculating employee hours.' + 
+  ' Visitors and employees have unique IDs that are used to sign in and sign out from the Innovation Center. Their time spent in' + 
+  ' the Innovation Center will be analyzed and displayed in an admin portal.  Employee hours will also be used to create timesheets' + 
+  ' for employee compensation. Visitors without a unique ID will also be able to sign in and sign out using their own name.',
+  paragraphThree: ' ',
+  resume: 'https://drive.google.com/file/d/1Oys4svWeMUfWEVegOQsTPROaeVntVK5j/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'mockup.png',
+    title: 'Low-Fidelity Mock-up',
+    info: 'Our low-fidelity mock-up was heavily inspired by websites such as Twitter and LinkedIn. We liked how simple and minimalistic those websites were.' + 
+    ' Ease of use was also one of our primary concerns. All of the buttons on the right side of the UI controls what is seen within the main feed section which is ' + 
+    'the center of the screen. The right side will contain statistics pertaining to the user.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
+  cta: ' ',
   btn: '',
   email: '',
 };
@@ -90,5 +78,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
